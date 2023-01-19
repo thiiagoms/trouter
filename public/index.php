c2750607a44a92d735be7f70ed8b9fb9ccadbe9e
thiiagoms <thiagom.devsec@gmail.com>
2023-01-19 20:36:29 -0300

    feat: Add trouter examples :egg:

--- public/index.php
+++ public/index.php
@@ -5,5 +5,19 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use TRouter\Controllers\HelloWorldController;
+use TRouter\Http\Router;
 
-echo HelloWorldController::hello();
+$router = new Router();
+
+$router->get('/', function () {
+    echo 'Hello World from callback';
+});
+
+$router->get('/static-method', HelloWorldController::class, 'testStatic');
+$router->get('/instance-method', HelloWorldController::class, 'testInstance');
+
+$router->post('/', function () {
+    echo 'Hello World from POST callback';
+});
+
+$router->run();
